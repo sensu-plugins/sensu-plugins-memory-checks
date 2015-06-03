d4afb79c2354531fd7b42d19949d51493ad49c84
Matt Jones <mattjones@yieldbot.com>
2015-06-03 11:51:29 -0400

    deploy

--- lib/sensu-plugins-memory-checks.rb
+++ lib/sensu-plugins-memory-checks.rb
@@ -1,15 +1,1 @@
-
 require 'sensu-plugins-memory-checks/version'
-
-# Load the defaults
-
-#
-# Default class
-#
-module SensuPluginsMemoryChecks
-  class << self
-  end
-
-  class << self
-  end
-end
