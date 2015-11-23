aec8b9bde8fc300229c46f7f8fb4eed62a16fa9c
Mathias Bogaert <mathias.bogaert@gmail.com>
2015-11-23 17:33:33 +0000

    Simplify the script by removing the filename and using File.basename.

--- bin/check-memory-percent.rb
+++ bin/check-memory-percent.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 
 bin_dir = File.expand_path(File.dirname(__FILE__))
-shell_script_path = File.join(bin_dir, 'check-memory-percent.sh')
+shell_script_path = File.join(bin_dir, File.basename($0, '.rb') + '.sh')
 
 exec shell_script_path, *ARGV
