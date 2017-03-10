d86f5b521da3ad2a79bde23c6fc2be9e6f8d4640
Ben Abrams <me@benabrams.it>
2017-03-10 00:04:34 -0800

    [WIP] fixes #43 - switches `free` to read from `/proc/swaps` which is more platform agnostic

--- bin/check-swap-percent.rb
+++ bin/check-swap-percent.rb
@@ -36,6 +36,13 @@ class CheckSWAP < Sensu::Plugin::Check::CLI
          proc: proc(&:to_i),
          default: 80
 
+  option :megabytes,
+         short: '-m',
+         long: '--megabytes',
+         description: 'alert on mbs rather than %, defaults to false',
+         boolean: true,
+         default: false
+
   option :required,
          short: '-r',
          long: '--required',
@@ -43,37 +50,75 @@ class CheckSWAP < Sensu::Plugin::Check::CLI
          boolean: true,
          default: false
 
+  def get_swaps
+    swaps = {}
+    begin
+      File.open("/proc/swaps", "r") do |f|
+        f.each_with_index do |line, index|
+          if index == 0
+            $fields = line.squeeze(' ').gsub("\t", ' ').split(' ')
+          else
+            # p index
+            key = line.split(' ')[0]
+            swaps[key] = {}
+            data = line.squeeze(' ').gsub("\t", ' ').split(' ') # unless index == 0
+            $fields.each_with_index do |f,i|
+              # p "fields: #{f},#{data[i]}"
+              swaps[key][f] = data[i]
+            end
+          end
+        end
+      end
+    rescue Errno::ENOENT
+      unknown 'Unable to find /proc/swaps please report a bug with information about your OS'
+    end
+    swaps
+  end
+
+  def get_percent(used, size)
+    (100 * (used.to_i / size.to_f)).round(2)
+  end
+
+  def bytes_to_mbs(value)
+    (value /= 1024 * 1024)
+  end
+
   def run
     # Get output from free command
-    output_swap = `free -m | grep "Swap"`
-
-    # Prepare total and used swap
-    total_swap = output_swap.split(' ')[1].to_i
-    used_swap = output_swap.split(' ')[2].to_i
-
-    # Check if swap exists
-    if total_swap.zero?
+    # output_swap = `free -m | grep "Swap"`
+    crits = []
+    warns = []
+    swaps = get_swaps
+    if swaps.empty?
       if config[:required]
-        critical 'ERROR: SWAP is missing on this machine'
+        critical 'ERROR: you said I should fail if swap is not present!'
       else
-        print 'There is no SWAP configured'
-        exit
+        unknown 'Unable to find swaps...bug?'
+      end
+    end
+    swaps.each do |o,k|
+      used_swap = k['Used']
+      total_swap = k['Size']
+      if config[:megabytes]
+        mbs = bytes_to_mbs(used_swap.to_f)
+        # crits << get_percent(k['Used'], k['Size'])
+        crits << "#{o}: #{mbs}" if mbs >= config[:warn]
+        warns << "#{o}: #{mbs}" if mbs >= config[:warning]
+      else
+        # we must want percentages
+        percent = get_percent(used_swap, total_swap)
+        crits << "#{o}: #{percent}" if percent >= config[:critical]
+        warns << "#{o}: #{percent}" if percent >= config[:warn]
       end
     end
 
-    # Prepare output message
-    output = "#{used_swap}/#{total_swap}"
-
-    # Calculate round percent from used and total
-    swapp = (100.0 * (used_swap.nonzero? || 1) / total_swap).round(0)
-
-    # Check conditions from config
-    if swapp >= config[:critical]
-      critical "#{swapp}% ; #{output}\n"
-    elsif swapp >= config[:warn]
-      warning "#{swapp}% ; #{output}\n"
+    # alert, or not?
+    if crits.any?
+      critical "swap limits exceded critical thresholds: #{crits}"
+    elsif warns.any?
+      warning "swap limits exceded warning thresholds: #{warns}"
     else
-      ok "#{swapp}% ; #{output}\n"
+      ok 'all swaps are within expected thresholds'
     end
   end
 end
