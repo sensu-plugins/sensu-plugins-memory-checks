11c3ded1d0c6160a89d9b57019409f1c303c53d5
Alex Werle Baulé <alexwbaule@gmail.com>
2017-03-30 16:41:21 -0300

    Code refactor using only /proc/meminfo

--- bin/check-memory-percent.rb
+++ bin/check-memory-percent.rb
@@ -1,6 +1,68 @@
 #!/usr/bin/env ruby
 
-bin_dir = File.expand_path(File.dirname(__FILE__))
-shell_script_path = File.join(bin_dir, File.basename($PROGRAM_NAME, '.rb') + '.sh')
+#  encoding: UTF-8
+#
+# Checks Memory usage as a % of the total Memory
+#
+# Date: 2017-03-30
+# Author: Alex Werle Baule <alexwbaule@gmail.com>
+#
+# PLATFORMS:
+#   Linux
+#
+# DEPENDENCIES:
+#   gem: sensu-plugin
+#
+# USAGE:
+#   check-memory-percent.rb -w warn_percent -c critical_percent
+#
+# LICENSE:
+#   Copyright 2017 Alex Werle Baule <alexwbaule@gmail.com>
+#   Released under the same terms as Sensu (the MIT license); see LICENSE
+#   for details.
 
-exec shell_script_path, *ARGV
+require 'sensu-plugin/check/cli'
+
+class CheckMemoryPercent < Sensu::Plugin::Check::CLI
+  option :warning,
+         short: '-w warning',
+         default: 80,
+	 proc: Proc.new { |w| w.to_i }
+
+  option :critical,
+         short: '-c critical',
+         default: 90,
+	 proc: Proc.new { |c| c.to_i }
+
+
+  def run
+    mem = metrics_hash
+    total_free = mem['free'] + mem['buffers'] + mem['cached']
+    total_used = mem['total'] - total_free
+    percent = (( total_used.to_f / mem['total'].to_f ) * 100).round(2)
+
+    if percent >= config[:critical]
+	critical percent.to_s + "%"
+    elsif percent >= config[:warning]
+	warning percent.to_s + "%"
+    else
+       ok percent.to_s + "%"
+    end
+  end
+
+  def metrics_hash
+    mem = {}
+    
+    meminfo_output.each_line do |line|
+      mem['total']     = line.split(/\s+/)[1].to_i if line =~ /^MemTotal/
+      mem['free']      = line.split(/\s+/)[1].to_i if line =~ /^MemFree/
+      mem['buffers']   = line.split(/\s+/)[1].to_i if line =~ /^Buffers/
+      mem['cached']    = line.split(/\s+/)[1].to_i if line =~ /^Cached/
+    end
+    mem
+  end
+
+  def meminfo_output
+    File.open('/proc/meminfo', 'r')
+  end
+end
--- bin/check-swap.rb
+++ bin/check-swap.rb
@@ -1,6 +1,48 @@
 #!/usr/bin/env ruby
 
-bin_dir = File.expand_path(File.dirname(__FILE__))
-shell_script_path = File.join(bin_dir, File.basename($PROGRAM_NAME, '.rb') + '.sh')
+# Looking at vmstat source code in sysinfo.c , 
+# the kb_swap_used is kb_swap_total - kb_swap_free,
+# so, lets do the same thing.
 
-exec shell_script_path, *ARGV
+require 'sensu-plugin/check/cli'
+
+class CheckSwap < Sensu::Plugin::Check::CLI
+  option :warning,
+         short: '-w warning',
+         default: 0,
+     proc: Proc.new { |w| w.to_i }
+
+  option :critical,
+         short: '-c critical',
+         default: 0,
+     proc: Proc.new { |c| c.to_i }
+
+
+
+  def run
+    mem = metrics_hash
+    used_mem = (( mem['total'] - mem['free'] ) / 1024.0 ).round(0)
+
+    if used_mem >= config[:critical]
+    critical used_mem.to_s + "MB"
+    elsif used_mem >= config[:warning]
+    warning used_mem.to_s + "MB"
+    else
+       ok used_mem.to_s + "MB"
+    end
+  end
+
+  def metrics_hash
+    mem = {}
+
+    meminfo_output.each_line do |line|
+      mem['free']      = line.split(/\s+/)[1].to_i if line =~ /^SwapFree/
+      mem['total']   = line.split(/\s+/)[1].to_i if line =~ /^SwapTotal/
+    end
+    mem
+  end
+
+  def meminfo_output
+    File.open('/proc/meminfo', 'r')
+  end
+end
