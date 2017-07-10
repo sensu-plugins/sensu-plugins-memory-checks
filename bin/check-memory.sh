sensu-plugins/sensu-plugins-memory-checks

commit 0898b556a70e380d479fb5c02904655d06daa4ee

Validate that both warning and critical values are provided (#62)
* Validate that both warning and critical values are provided

* Update CHANGELOG.md

bin/check-memory.sh
@@ -41,8 +41,10 @@ if [ "$hlp" = "yes" ]; then
   exit 0
 fi
 
-WARN=${WARN:=0}
-CRIT=${CRIT:=0}
+if [ -z "$WARN"] || [ -z "$CRIT" ]; then
+  echo "You must provide both warning and critical values"
+  exit 3
+fi
 
 set -o pipefail
 FREE_MEMORY=$(free -m | grep buffers/cache | awk '{ print $4 }')

commit fedbcf0770af8ada9f99a594a4b8fc48ddda8ccf

fixes #63 which was caused in #62

bin/check-memory.sh
@@ -41,7 +41,7 @@ if [ "$hlp" = "yes" ]; then
   exit 0
 fi
 
-if [ -z "$WARN"] || [ -z "$CRIT" ]; then
+if [ -z "$WARN" ] || [ -z "$CRIT" ]; then
   echo "You must provide both warning and critical values"
   exit 3
 fi

lib/sensu-plugins-memory-checks/version.rb
@@ -2,7 +2,7 @@ module SensuPluginsMemoryChecks
   module Version
     MAJOR = 3
     MINOR = 0
-    PATCH = 0
+    PATCH = 1
 
     VER_STRING = [MAJOR, MINOR, PATCH].compact.join('.')
   end